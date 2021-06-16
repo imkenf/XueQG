853703d5fd42ef6843524d7f8e447df8a74224c5
imkenf <52485888+imkenf@users.noreply.github.com>
2021-06-16 08:55:03 +0800

    Create urls.py

---
+++ func/urls.py
@@ -0,0 +1,23 @@
+user_Info_url = "https://pc-api.xuexi.cn/open/api/user/info"
+user_totalScore_url = "https://pc-api.xuexi.cn/open/api/score/get"
+user_todayTotalScore_url = "https://pc-api.xuexi.cn/open/api/score/today/query"
+user_rateScore_url = "https://pc-api.xuexi.cn/open/api/score/today/queryrate"
+
+article_url_list = ["https://www.xuexi.cn/lgdata/35il6fpn0ohq.json",
+                    "https://www.xuexi.cn/lgdata/45a3hac2bf1j.json",
+                    "https://www.xuexi.cn/lgdata/1ajhkle8l72.json",
+                    "https://www.xuexi.cn/lgdata/1ahjpjgb4n3.json",
+                    "https://www.xuexi.cn/lgdata/1je1objnh73.json",
+                    "https://www.xuexi.cn/lgdata/1kvrj9vvv73.json",
+                    "https://www.xuexi.cn/lgdata/17qonfb74n3.json",
+                    "https://www.xuexi.cn/lgdata/1i30sdhg0n3.json"]
+ 
+video_url_list = ["https://www.xuexi.cn/lgdata/3j2u3cttsii9.json",
+                  "https://www.xuexi.cn/lgdata/1novbsbi47k.json",
+                  "https://www.xuexi.cn/lgdata/31c9ca1tgfqb.json",
+                  "https://www.xuexi.cn/lgdata/1oajo2vt47l.json",
+                  "https://www.xuexi.cn/lgdata/18rkaul9h7l.json",
+                  "https://www.xuexi.cn/lgdata/2qfjjjrprmdh.json",
+                  "https://www.xuexi.cn/lgdata/3o3ufqgl8rsn.json",
+                  "https://www.xuexi.cn/lgdata/525pi8vcj24p.json",
+                  "https://www.xuexi.cn/lgdata/1742g60067k.json"] 
